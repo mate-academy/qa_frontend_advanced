dfbfc5783bb33e805f1314a15c2781f9e8c183f9
Danylo Mysak <danmysak@gmail.com>
2019-12-13 14:03:44 +0200

    Making ESLint tolerate Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,21 @@
 module.exports = {
   extends: '@mate-academy/eslint-config-react',
+  overrides: [
+    {
+      files: [
+        "**/*.test.js"
+      ],
+      env: {
+        jest: true
+      },
+      plugins: ["jest"],
+      rules: {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
+      }
+    }
+  ]
 };
